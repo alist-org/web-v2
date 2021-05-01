3168c7617ffd843902ac3465555f0ffae5412d27
微凉 <927625802@qq.com>
2021-05-01 20:41:47 +0800

    :bookmark: release 1.0.4

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 import { getUrl } from "./get_url"
 
-export const VERSION = 'v1.0.3'
+export const VERSION = 'v1.0.4'
 
 export const fileExtensions: { [key: string]: string } = {
   exe: 'windows',
