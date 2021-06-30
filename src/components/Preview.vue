bdf593c6fef33fb41cd8f3997fa7d73baa6f0a27
nfyb <hfsimply@gmail.com>
2021-06-30 14:03:33 +0800

    Update Preview.vue

--- src/components/Preview.vue
+++ src/components/Preview.vue
@@ -140,7 +140,7 @@ export default defineComponent({
         }
         dp=new DPlayer(videoOptions)
       }else{
-        //多清晰度视频
+        //多清晰度视频支持
         videoPreviewPost(store.state.drive, file.value.file_id).then(resp => {
           const res = resp.data
           if (res.code === 200) {
