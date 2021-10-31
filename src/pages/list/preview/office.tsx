00c5f7311c3cd89341a5c4415393ef371f611601
微凉 <927625802@qq.com>
2021-10-31 18:44:08 +0800

    :sparkles: office preview

---
+++ src/pages/list/preview/office.tsx
@@ -0,0 +1,52 @@
+import { Box } from "@chakra-ui/layout";
+import React, { useEffect } from "react";
+import { useLocation } from "react-router";
+import { FileProps } from "..";
+import useDownLink from "../../../hooks/useDownLink";
+import request from "../../../utils/public";
+
+export const type = 2;
+export const exts = [];
+
+declare namespace aliyun {
+  class Config {
+    setToken(token: { token: string }): any;
+  }
+  function config({ mount: Element, url: string }): Config;
+}
+
+const Office = ({ file }: FileProps) => {
+  const { pathname } = useLocation();
+  const link = useDownLink();
+  const refresh = () => {
+    if (file.driver === "AliDrive") {
+      request.post("preview", { path: pathname }).then((resp) => {
+        const res = resp.data;
+        const docOptions = aliyun.config({
+          mount: document.querySelector("#office-preview"),
+          url: res.data.preview_url, //设置文档预览URL地址。
+        });
+        docOptions.setToken({ token: res.data.access_token });
+      });
+    }
+  };
+  useEffect(() => {
+    refresh();
+  }, [pathname]);
+  return (
+    <Box w="full" h="75vh" id="office-preview">
+      {file.driver !== "AliDrive" && (
+        <iframe
+          width="100%"
+          height="100%"
+          src={`https://view.officeapps.live.com/op/view.aspx?src=${encodeURIComponent(
+            link
+          )}`}
+          frameBorder="0"
+        />
+      )}
+    </Box>
+  );
+};
+
+export default Office;
