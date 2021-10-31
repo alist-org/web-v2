f63b6791129dc66772e3971ef88c37b117fdfba6
微凉 <927625802@qq.com>
2021-10-31 19:19:30 +0800

    :bug: host

--- src/hooks/useDownLink.ts
+++ src/hooks/useDownLink.ts
@@ -2,9 +2,9 @@ import { useLocation } from "react-router-dom";
 
 const useDownLink = (proxy = false) => {
   const location = useLocation();
-  let host = window.location.host + "/";
-  if (import.meta.env.MODE === "development") {
-    host = import.meta.env.VITE_SERVER_URL as string;
+  let host = import.meta.env.VITE_SERVER_URL as string;
+  if(host === "/"){
+    host = window.location.origin + "/";
   }
   if (proxy) {
     return `${host}p${location.pathname}`;
