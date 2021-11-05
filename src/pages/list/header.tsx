9007a65b24a61eee2efa644744b6908c4f9ab5b0
微凉 <927625802@qq.com>
2021-11-05 12:32:01 +0800

    :bug: copy for http

--- src/pages/list/header.tsx
+++ src/pages/list/header.tsx
@@ -1,4 +1,12 @@
-import { Flex, Heading, HStack, Image, Icon, useToast, Spinner } from "@chakra-ui/react";
+import {
+  Flex,
+  Heading,
+  HStack,
+  Image,
+  Icon,
+  useToast,
+  Spinner,
+} from "@chakra-ui/react";
 import React, { useContext } from "react";
 import { IContext } from ".";
 import { FaListUl } from "react-icons/fa";
@@ -7,17 +15,26 @@ import { BsFillGridFill } from "react-icons/bs";
 import { useTranslation } from "react-i18next";
 import { Link, useLocation } from "react-router-dom";
 import useDownLink from "../../hooks/useDownLink";
+import { copyToClip } from "../../utils/copy-clip";
 
 const Header = () => {
   const { t } = useTranslation();
-  const link = useDownLink()
+  const link = useDownLink();
   const toast = useToast();
   const { show, setShow, type, getSetting } = useContext(IContext);
   return (
     <Flex px="2" py="2" justify="space-between" w="full">
       <Link to="/">
         {getSetting("logo").includes("http") ? (
-          <Image fallback={<Spinner color={getSetting("icon color") || "teal.300"} />} rounded="lg" h="44px" w="auto" src={getSetting("logo")} />
+          <Image
+            fallback={
+              <Spinner color={getSetting("icon color") || "teal.300"} />
+            }
+            rounded="lg"
+            h="44px"
+            w="auto"
+            src={getSetting("logo")}
+          />
         ) : (
           <Heading>{getSetting("logo")}</Heading>
         )}
@@ -29,7 +46,11 @@ const Header = () => {
             boxSize={6}
             as={AiTwotoneCopy}
             onClick={() => {
-              navigator.clipboard.writeText(link);
+              if (navigator.clipboard) {
+                navigator.clipboard.writeText(link);
+              } else {
+                copyToClip(link);
+              }
               toast({
                 title: t("copied"),
                 status: "success",
