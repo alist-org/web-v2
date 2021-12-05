81076588c485fde931b4d0c8b764915fad788c4c
微凉 <927625802@qq.com>
2021-12-05 13:52:31 +0800

    :racehorse: change vite build target

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import pluginRewriteAll from 'vite-plugin-rewrite-all';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh(), pluginRewriteAll()],
+  build: {
+    target: 'es2015',
+  }
 })
