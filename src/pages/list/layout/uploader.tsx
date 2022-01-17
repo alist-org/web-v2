15daaff5d54ad3a55452213d1efd7dd7567fa11b
微凉 <i@nn.ci>
2022-01-17 15:25:11 +0800

    :sparkles: show upload speed and remaining time

--- src/pages/list/layout/uploader.tsx
+++ src/pages/list/layout/uploader.tsx
@@ -19,10 +19,14 @@ import { useLocation } from "react-router-dom";
 import request from "../../../utils/public";
 import { IContext, Resp } from "../context";
 import bus from "../../../utils/event-bus";
+import { getFileSize } from "../../../utils/file";
 
 export interface UploaderHandle {
   upload: () => void;
 }
+
+let oldTimestamp = new Date().valueOf();
+let oldLoaded = 0;
 
 const Uploader = forwardRef<UploaderHandle>((_props, ref) => {
   useImperativeHandle(ref, () => ({
@@ -71,7 +75,25 @@ const Uploader = forwardRef<UploaderHandle>((_props, ref) => {
                 if (progressEvent.lengthComputable) {
                   const complete =
                     ((progressEvent.loaded / progressEvent.total) * 100) | 0;
+                  console.log('complete',complete);
                   setProgress(complete);
+
+                  const timestamp = new Date().valueOf();
+                  const duration = (timestamp - oldTimestamp) / 1000;
+                  if (duration > 1) {
+                    const loaded = progressEvent.loaded - oldLoaded;
+                    const speed = loaded / duration;
+                    const remain = progressEvent.total - progressEvent.loaded;
+                    const remainTime = remain / speed;
+                    setMsg(
+                      `${getFileSize(speed)}/s  (${t(
+                        "about remaining"
+                      )} ${Math.round(remainTime)}${t("s")})`
+                    );
+                    oldTimestamp = timestamp;
+                    oldLoaded = progressEvent.loaded;
+                  }
+
                   if (complete === 100) {
                     setMsg("Back-end processing");
                   }
@@ -95,28 +117,29 @@ const Uploader = forwardRef<UploaderHandle>((_props, ref) => {
             });
         }}
       />
-      <ScaleFade initialScale={0.9} in={isOpen}>
-        <Box
-          zIndex={99}
-          w={{ base: "80vw", md: "40vw" }}
-          position="fixed"
-          bottom="20px"
-          right="20px"
-          bgColor={bgColor}
+      {/* <ScaleFade initialScale={0.9} in={isOpen}> */}
+      <Box
+        zIndex={99}
+        w={{ base: "80vw", md: "40vw" }}
+        position="fixed"
+        bottom="20px"
+        right="20px"
+        bgColor={bgColor}
+        rounded="lg"
+        p={4}
+        display={isOpen ? "block" : "none"}
+      >
+        <Heading mb={2} fontSize={20}>
+          {t(msg)}...
+        </Heading>
+        <Progress
+          hasStripe={true}
+          isAnimated={true}
           rounded="lg"
-          p={4}
-        >
-          <Heading mb={2} fontSize={20}>
-            {t(msg)}...
-          </Heading>
-          <Progress
-            hasStripe={true}
-            isAnimated={true}
-            rounded="lg"
-            value={progress}
-          />
-        </Box>
-      </ScaleFade>
+          value={progress}
+        />
+      </Box>
+      {/* </ScaleFade> */}
     </Box>
   );
 });
