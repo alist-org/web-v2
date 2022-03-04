3c9dab13a7f7a13e07a2280695e02b110c871443
grey.lu <luwenjiechn@qq.com>
2022-03-04 17:08:34 +0800

    feat: add resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -2,22 +2,31 @@ import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 import pluginRewriteAll from "vite-plugin-rewrite-all";
 import { useDynamicPublicPath } from "vite-plugin-dynamic-publicpath";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "",
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "src"),
+    },
+  },
+
   plugins: [
     reactRefresh(),
     pluginRewriteAll(),
     useDynamicPublicPath(),
   ],
+
   build: {
     target: "es2015",
   },
+  
   define: {
     "process.env": {},
   },
-  server:{
-    host: "0.0.0.0"
-  }
+  server: {
+    host: "0.0.0.0",
+  },
 });
