55135917efb9062f961a7b0b8e4aa89148471b5c
Xhofe <i@nn.ci>
2022-04-08 19:57:35 +0800

    feat: add search button

--- src/pages/list/layout/header.tsx
+++ src/pages/list/layout/header.tsx
@@ -9,8 +9,9 @@ import {
   Tooltip,
   useColorModeValue,
   Box,
+  SlideFade,
 } from "@chakra-ui/react";
-import React, { useContext, useRef } from "react";
+import React, { useContext, useRef, useState } from "react";
 import { IContext } from "../context";
 import { FaListUl } from "react-icons/fa";
 // import { AiTwotoneCopy } from "react-icons/ai";
@@ -25,6 +26,7 @@ import { Link, useLocation } from "react-router-dom";
 import { copyToClip } from "../../../utils/copy-clip";
 import useFileUrl from "../../../hooks/useFileUrl";
 import Uploader, { UploaderHandle } from "./uploader";
+import Search from "./search";
 
 const Header = () => {
   const { t } = useTranslation();
@@ -46,16 +48,14 @@ const Header = () => {
     logos.split(",").shift(),
     logos.split(",").pop()
   ) as string;
-  const { pathname } = useLocation();
   const uploadRef = useRef<UploaderHandle>(null);
+  const [isSearch, setIsSearch] = useState(false);
   return (
     <Flex className="header" px="2" py="2" justify="space-between" w="full">
       <Link to="/" className="logo">
         {logo.includes("http") ? (
           <Image
-            fallback={
-              <Spinner color={getSetting("icon color") || "#1890ff"} />
-            }
+            fallback={<Spinner color={getSetting("icon color") || "#1890ff"} />}
             rounded="lg"
             h="44px"
             w="auto"
@@ -66,113 +66,113 @@ const Header = () => {
         )}
       </Link>
       <HStack className="buttons" spacing="2">
-        {type === "file" && (
-          <Tooltip
-            shouldWrapChildren
-            hasArrow
-            placement="bottom"
-            label={t("Download")}
-          >
-            <Icon
-              cursor="pointer"
-              boxSize={6}
-              as={BsFillArrowDownCircleFill}
-              onClick={() => {
-                if (type === "file") {
-                  let url = fileUrl();
-                  window.open(url, "_blank");
-                  return;
-                }
-                // if (multiSelect) {
-                //   downPack(selectFiles);
-                //   return;
-                // }
-                // if (type === "folder") {
-                //   downPack(files);
-                //   return;
-                // }
-              }}
-            />
-          </Tooltip>
-        )}
-        {type === "folder" &&
-          !getSetting("no upload").split(",").includes(meta.driver) &&
-          (meta.upload || loggedIn) && (
-            <Box>
+        {type === "folder" && <Search isSearch={isSearch} setIsSearch={setIsSearch} />}
+        {!isSearch&&<SlideFade in={!isSearch} offsetX="20px" offsetY={0}>
+          <HStack spacing="2">
+            {type === "file" && (
               <Tooltip
                 shouldWrapChildren
                 hasArrow
                 placement="bottom"
-                label={t("Upload file")}
+                label={t("Download")}
               >
                 <Icon
                   cursor="pointer"
                   boxSize={6}
-                  as={BsFillArrowUpCircleFill}
+                  as={BsFillArrowDownCircleFill}
                   onClick={() => {
-                    uploadRef.current!.upload();
+                    if (type === "file") {
+                      let url = fileUrl();
+                      window.open(url, "_blank");
+                      return;
+                    }
                   }}
                 />
               </Tooltip>
-              <Uploader ref={uploadRef} />
-            </Box>
-          )}
-        {type !== "error" && (
-          <Tooltip
-            shouldWrapChildren
-            hasArrow
-            placement="bottom"
-            label={t("Copy direct link")}
-          >
-            <Icon
-              cursor="pointer"
-              boxSize={6}
-              as={IoIosCopy}
-              onClick={() => {
-                let content = "";
-                if (type === "file") {
-                  content = fileUrl();
-                } else {
-                  let files_ = files;
-                  if (multiSelect) {
-                    files_ = selectFiles;
-                  }
-                  content = files_
-                    .filter((file) => file.type !== 1)
-                    .map((file) => {
-                      return fileUrl(file);
-                    })
-                    .join("\n");
-                }
-                copyToClip(content);
-                toast({
-                  title: t("Copied"),
-                  status: "success",
-                  duration: 3000,
-                  isClosable: true,
-                });
-              }}
-            />
-          </Tooltip>
-        )}
-        <Tooltip
-          shouldWrapChildren
-          hasArrow
-          placement="bottom"
-          label={t("switch to layout view", {
-            layout: t(show === "list" ? "grid" : "list"),
-          })}
-        >
-          <Icon
-            boxSize={6}
-            cursor="pointer"
-            onClick={() => {
-              setShow!(show === "list" ? "grid" : "list");
-              localStorage.setItem("show", show === "list" ? "grid" : "list");
-            }}
-            as={show === "list" ? BsFillGridFill : FaListUl}
-          />
-        </Tooltip>
+            )}
+            {type === "folder" &&
+              !getSetting("no upload").split(",").includes(meta.driver) &&
+              (meta.upload || loggedIn) && (
+                <Box>
+                  <Tooltip
+                    shouldWrapChildren
+                    hasArrow
+                    placement="bottom"
+                    label={t("Upload file")}
+                  >
+                    <Icon
+                      cursor="pointer"
+                      boxSize={6}
+                      as={BsFillArrowUpCircleFill}
+                      onClick={() => {
+                        uploadRef.current!.upload();
+                      }}
+                    />
+                  </Tooltip>
+                  <Uploader ref={uploadRef} />
+                </Box>
+              )}
+            {type !== "error" && (
+              <Tooltip
+                shouldWrapChildren
+                hasArrow
+                placement="bottom"
+                label={t("Copy direct link")}
+              >
+                <Icon
+                  cursor="pointer"
+                  boxSize={6}
+                  as={IoIosCopy}
+                  onClick={() => {
+                    let content = "";
+                    if (type === "file") {
+                      content = fileUrl();
+                    } else {
+                      let files_ = files;
+                      if (multiSelect) {
+                        files_ = selectFiles;
+                      }
+                      content = files_
+                        .filter((file) => file.type !== 1)
+                        .map((file) => {
+                          return fileUrl(file);
+                        })
+                        .join("\n");
+                    }
+                    copyToClip(content);
+                    toast({
+                      title: t("Copied"),
+                      status: "success",
+                      duration: 3000,
+                      isClosable: true,
+                    });
+                  }}
+                />
+              </Tooltip>
+            )}
+            <Tooltip
+              shouldWrapChildren
+              hasArrow
+              placement="bottom"
+              label={t("switch to layout view", {
+                layout: t(show === "list" ? "grid" : "list"),
+              })}
+            >
+              <Icon
+                boxSize={6}
+                cursor="pointer"
+                onClick={() => {
+                  setShow!(show === "list" ? "grid" : "list");
+                  localStorage.setItem(
+                    "show",
+                    show === "list" ? "grid" : "list"
+                  );
+                }}
+                as={show === "list" ? BsFillGridFill : FaListUl}
+              />
+            </Tooltip>
+          </HStack>
+        </SlideFade>}
       </HStack>
     </Flex>
   );
