aad05a8e6150ba2964a783483df1247ef75018f7
Noah Hsu <i@nn.ci>
2022-05-19 21:54:16 +0800

    feat: customize pdf viewer (Xhofe/alist#1109)

--- src/pages/list/preview/pdf.tsx
+++ src/pages/list/preview/pdf.tsx
@@ -1,17 +1,20 @@
 import { Center } from "@chakra-ui/react";
-import React from "react";
+import React, { useContext } from "react";
+import { IContext } from "../context";
 
 export const type = -1;
 export const exts = [];
 
 const Pdf = ({ url, unfold }: { url: string; unfold: boolean }) => {
+  const { getSetting } = useContext(IContext)
+  const pdfPreviewUrl = getSetting("pdf viewer url") || "https://alist-org.github.io/pdf.js/web/viewer.html?file=$url";
   return (
     <Center w="full" h="full" className="pdf-preview-box">
       <iframe
         title="pdf-viewer"
         width="100%"
         height="100%"
-        src={`https://alist-org.github.io/pdf.js/web/viewer.html?file=${url}`}
+        src={pdfPreviewUrl.replace("$url", url)}
       />
     </Center>
   );
