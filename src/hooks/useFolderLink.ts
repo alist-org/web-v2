3e25876582762d91c0ff7bd4af401f5edc76e8ce
Noah Hsu <i@nn.ci>
2022-07-21 20:10:50 +0800

    fix: error link while subfolder is empty (fix: alist-org/alist#1356)

--- src/hooks/useFolderLink.ts
+++ src/hooks/useFolderLink.ts
@@ -7,21 +7,24 @@ const useFolderLink = (proxy = false) => {
   let { pathname } = useLocation();
   let host = import.meta.env.VITE_SERVER_URL as string;
   if (host === "/") {
-    host = window.location.origin + "/";
+    host = window.location.origin;
   }
   let path = pathname.endsWith("/") ? pathname.slice(0, -1) : pathname;
   if (type === "file") {
     path = path.substring(0, path.lastIndexOf("/"));
   }
-  let subfolder = ""
+  let subfolder = "";
   if (window.ALIST.subfolder) {
     subfolder = window.ALIST.subfolder;
   }
   if (subfolder.endsWith("/")) {
     subfolder = subfolder.substring(0, subfolder.length - 1);
   }
-  if (subfolder.startsWith("/")) {
-    subfolder = subfolder.substring(1);
+  if (!subfolder.startsWith("/")) {
+    subfolder = "/" + subfolder;
+  }
+  if (subfolder === "/") {
+    subfolder = "";
   }
   if (proxy) {
     return `${host}${subfolder}/p${path}`;
